ccc12a4c1f31ed97795260b1c41e48f55bf8b12d
aabccd021 <aabccd021@gmail.com>
2022-11-01 15:51:54 +0700

    checkpoint

--- dist/install-nix.sh
+++ dist/install-nix.sh
@@ -1,22 +1,20 @@
 #!/usr/bin/env bash
 
-set -e
+set -euo pipefail
 
-{
-  # Create a temporary workdir
-  workdir=$(mktemp -d)
-  trap 'rm -rf "$workdir"' EXIT
+# Create a temporary workdir
+workdir=$(mktemp -d)
+trap 'rm -rf "$workdir"' EXIT
 
-  curl_retries=5
-  while ! curl -sS -o "$workdir/install" -v --fail -L "https://raw.githubusercontent.com/cachix/install-nix-action/11f4ad19be46fd34c005a2864996d8f197fb51c6/install-nix.sh"
-  do
-    sleep 1
-    ((curl_retries--))
-    if [[ $curl_retries -le 0 ]]; then
-      echo "curl retries failed" >&2
-      exit 1
-    fi
-  done
+curl_retries=5
+while ! curl -sS -o "$workdir/install" -v --fail -L "https://raw.githubusercontent.com/cachix/install-nix-action/11f4ad19be46fd34c005a2864996d8f197fb51c6/install-nix.sh"
+do
+  sleep 1
+  ((curl_retries--))
+  if [[ $curl_retries -le 0 ]]; then
+    echo "curl retries failed" >&2
+    exit 1
+  fi
+done
 
-  INPUT_INSTALL_URL="https://releases.nixos.org/nix/nix-${NIX_VERSION}/install" bash "$workdir/install"
-}
+INPUT_INSTALL_URL="https://releases.nixos.org/nix/nix-${NIX_VERSION}/install" bash "$workdir/install"
