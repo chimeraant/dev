ec2f27b516a9bf7643912b4c51866a6bc4383fd0
aabccd021 <aabccd021@gmail.com>
2022-11-01 16:59:32 +0700

    allow script

--- dist/cleanup/index.js
+++ dist/cleanup/index.js
@@ -68,7 +68,6 @@ exports.c = {
     nixCachePath: '/tmp/nixcache',
     nixVersion: '2.11.0',
     direnvVersion: '2.32.1',
-    nixInstallScriptUrl: 'https://raw.githubusercontent.com/cachix/install-nix-action/11f4ad19be46fd34c005a2864996d8f197fb51c6/install-nix.sh',
     isNixStoreCacheHitStateKey: 'isNixStoreCacheHit',
     nixStoreKeyStateKey: 'nixStoreKey'
 };
