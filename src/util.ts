e6c70ba2bd8efa0a9262def417a4f054125e78dd
aabccd021 <aabccd021@gmail.com>
2022-11-02 17:02:43 +0700

    fix: cache and new install

--- src/util.ts
+++ src/util.ts
@@ -1,15 +1,31 @@
 import * as cache from '@actions/cache';
 import * as core from '@actions/core';
 import * as exec from '@actions/exec';
+import * as glob from '@actions/glob';
 import * as path from 'path';
 
 export const execScript = (scriptName: string, args?: string[]) =>
   exec.exec(path.join(path.dirname(__filename), scriptName), args);
 
-const cacheConfig = (cachePath: string, keyInput: string, restoreKeyInput: string) => {
-  const saveKey = core.getInput(keyInput);
+const nonEmptyStrOrElse = async (str: string, defaultStr: () => Promise<string>) =>
+  str !== '' ? str : await defaultStr();
+
+const nonEmptyArrOrElse = (arr: string[], defaultArr: string[]) =>
+  arr.length > 0 ? arr : defaultArr;
+
+const cacheConfig = async (
+  cachePath: string,
+  keyInput: string,
+  defaultKeyInput: () => Promise<string>,
+  restoreKeyInput: string,
+  defaultRestoreKeyInput: string[]
+) => {
   const cacheStateId = keyInput;
-  const restoreKeys = core.getInput(restoreKeyInput).split('\n');
+  const saveKey = await nonEmptyStrOrElse(core.getInput(keyInput), defaultKeyInput);
+  const restoreKeys = nonEmptyArrOrElse(
+    core.getInput(restoreKeyInput).split('\n'),
+    defaultRestoreKeyInput
+  );
   return {
     path: cachePath,
     key: saveKey,
@@ -33,13 +49,41 @@ const cacheConfig = (cachePath: string, keyInput: string, restoreKeyInput: strin
   };
 };
 
-export const nixCache = cacheConfig(
-  '/tmp/nixcache',
-  'nix-store-cache-key',
-  'nix-store-cache-restore-keys'
-);
-export const pnpmCache = cacheConfig(
-  '~/.local/share/pnpm/store/v3',
-  'pnpm-store-cache-key',
-  'pnpm-store-cache-restore-keys'
-);
+const nixStoreCacheKeyPrefix = `${process.env['RUNNER_OS']}-nix-store-`;
+
+const logAndHash = async (pattern: string) => {
+  const globber = await glob.create(core.getInput('files'));
+  const files = [];
+  for await (const file of globber.globGenerator()) {
+    files.push(file);
+  }
+  const hash = await glob.hashFiles(pattern);
+  core.info(`hashing files: ${files} => ${hash}`);
+  return hash;
+};
+
+export const getNixCache = () =>
+  cacheConfig(
+    '/tmp/nixcache',
+    'nix-store-cache-key',
+    async () => {
+      const hash = await logAndHash('[flake.nix][flake.lock]');
+      return `${nixStoreCacheKeyPrefix}${hash}`;
+    },
+    'nix-store-cache-restore-keys',
+    [nixStoreCacheKeyPrefix]
+  );
+
+const pnpmStoreCacheKeyPrefix = `${process.env['RUNNER_OS']}-pnpm-store-`;
+
+export const getPnpmCache = () =>
+  cacheConfig(
+    '~/.local/share/pnpm/store/v3',
+    'pnpm-store-cache-key',
+    async () => {
+      const hash = await logAndHash('**/pnpm-lock.yaml');
+      return `${pnpmStoreCacheKeyPrefix}${hash}`;
+    },
+    'pnpm-store-cache-restore-keys',
+    [pnpmStoreCacheKeyPrefix]
+  );
