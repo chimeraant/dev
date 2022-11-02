8ccc8534b38ec235ceb920c87ea54e5ff4bd2234
aabccd021 <aabccd021@gmail.com>
2022-11-02 18:58:20 +0700

    fix: remove file://

--- dist/import.sh
+++ dist/import.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 echo "::group::Importing to Nix Store"
 ls "$1" | wc -l
-nix copy --derivation -s --no-check-sigs --from "file://$1"
+nix copy -s --no-check-sigs --from "$1"
 echo "::endgroup::"
