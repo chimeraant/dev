621c2f970c36caf5c907638dc1eed686e4604610
aabccd021 <aabccd021@gmail.com>
2022-11-02 21:11:31 +0700

    fix: install direnv using nix

--- dist/install.sh
+++ dist/install.sh
@@ -7,12 +7,10 @@ set -euo pipefail
   export INPUT_EXTRA_NIX_CONFIG=
   export INPUT_INSTALL_OPTIONS=
   export INPUT_INSTALL_URL=
-  export INPUT_NIX_PATH=
+  export INPUT_NIX_PATH=nixpkgs=channel:nixos-unstable
   curl -sfL https://raw.githubusercontent.com/cachix/install-nix-action/master/install-nix.sh | bash
 
-  echo "::group::Installing direnv"
-  curl -sfL https://direnv.net/install.sh | bash
-  echo "::endgroup::"
+  nix-env -f '<nixpkgs>' -iA direnv
 
   LINE='eval "\$(direnv hook bash)"'
   FILE="$HOME/.profile"
