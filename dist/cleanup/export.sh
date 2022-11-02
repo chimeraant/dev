da1c18f12a109e0f15f7448b15c3f327cd33e971
aabccd021 <aabccd021@gmail.com>
2022-11-02 13:42:05 +0700

    fix: gc nix store

--- dist/cleanup/export.sh
+++ dist/cleanup/export.sh
@@ -2,7 +2,14 @@
 
 set -euo pipefail
 
-echo "::group::Optimising Nix Store"
+echo "::group::Garbage collecting nix store"
+# clear unused packages from restored cache.
+# for example, new cache should be smaller if less package were used, 
+# but will cache entire store from previous cache if it was not garbage collected
+nix-store --gc
+echo "::endgroup::"
+
+echo "::group::Optimising nix store"
 nix-store --optimise
 echo "::endgroup::"
 
