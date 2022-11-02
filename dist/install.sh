c6047bd20dca4509ff7900cb54a161970c8a4be9
aabccd021 <aabccd021@gmail.com>
2022-11-02 21:47:02 +0700

    fix: fix all

--- dist/install.sh
+++ dist/install.sh
@@ -11,10 +11,15 @@ set -euo pipefail
   GITHUB_PATH=/dev/null \
   bash <(curl -sfL https://raw.githubusercontent.com/cachix/install-nix-action/master/install-nix.sh)
 
-  nix-env -iA direnv -f 'channel:nixos-unstable'
 
-  LINE='eval "\$(direnv hook bash)"'
-  FILE="$HOME/.profile"
-  grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
-
+  echo "::group::Installing direnv"
+  if type -p direnv &>/dev/null ; then
+    echo "Skip installing direnv: already installed at $(type -p direnv)"
+  else
+    curl -sfL https://direnv.net/install.sh | bash
+    LINE='eval "\$(direnv hook bash)"'
+    FILE="$HOME/.profile"
+    grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+  fi
+  echo "::endgroup::"
 }
