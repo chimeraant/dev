02ad522b8e8252eeff32e52272bc3a5360768dde
aabccd021 <aabccd021@gmail.com>
2022-11-02 21:17:41 +0700

    fix: fix env var

--- dist/install.sh
+++ dist/install.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 
 {
 
-  export INPUT_EXTRA_NIX_CONFIG=
-  export INPUT_INSTALL_OPTIONS=
-  export INPUT_INSTALL_URL=
-  export INPUT_NIX_PATH="nixpkgs=channel:nixos-unstable"
+  INPUT_EXTRA_NIX_CONFIG= \
+  INPUT_INSTALL_OPTIONS= \
+  INPUT_INSTALL_URL= \
+  INPUT_NIX_PATH="nixpkgs=channel:nixos-unstable" \
   curl -sfL https://raw.githubusercontent.com/cachix/install-nix-action/master/install-nix.sh | bash
 
   nix-env -f '<nixpkgs>' -iA direnv
