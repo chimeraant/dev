406412cb6d7b936a94b6d260f6efd1a8c477e669
aabccd021 <aabccd021@gmail.com>
2022-11-02 17:19:27 +0700

    fix: fix install.sh

--- dist/install.sh
+++ dist/install.sh
@@ -3,6 +3,10 @@
 set -euo pipefail
 
 {
+  export INPUT_EXTRA_NIX_CONFIG=
+  export INPUT_INSTALL_OPTIONS=
+  export INPUT_INSTALL_URL=
+  export INPUT_NIX_PATH=
   curl -sfL https://raw.githubusercontent.com/cachix/install-nix-action/master/install-nix.sh | bash
 
   echo "::group::Installing direnv"
