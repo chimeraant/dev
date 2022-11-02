46448f53603f752866cfdd90af2cd144637e5913
aabccd021 <aabccd021@gmail.com>
2022-11-02 21:34:46 +0700

    fix: fix again

--- dist/install.sh
+++ dist/install.sh
@@ -8,8 +8,7 @@ set -euo pipefail
   INPUT_INSTALL_OPTIONS= \
   INPUT_INSTALL_URL= \
   INPUT_NIX_PATH= \
-  GITHUB_ENV= \
-  GITHUB_PATH= \
+  GITHUB_PATH=/dev/null \
   bash <(curl -sfL https://raw.githubusercontent.com/cachix/install-nix-action/master/install-nix.sh)
 
   nix-env --install --attr direnv -file 'channel:nixos-unstable' 
