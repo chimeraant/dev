573265aa2130d0a2fe08abf080ab3eb12a6bfbe0
aabccd021 <aabccd021@gmail.com>
2022-11-02 22:18:19 +0700

    docs: add docs

--- dist/install.sh
+++ dist/install.sh
@@ -8,6 +8,9 @@ set -euo pipefail
   INPUT_INSTALL_OPTIONS= \
   INPUT_INSTALL_URL= \
   INPUT_NIX_PATH= \
+  # ignore GITHUB_PATH for local installation
+  # GITHUB_PATH from the downloaded script is not added to path anyway
+  # path export is instead done using js toolkit
   GITHUB_PATH=/dev/null \
   bash <(curl -sfL https://raw.githubusercontent.com/cachix/install-nix-action/master/install-nix.sh)
 
