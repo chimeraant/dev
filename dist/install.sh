f034558fc93bd05ee592e129e9e5040d33946c7e
aabccd021 <aabccd021@gmail.com>
2022-11-03 15:58:24 +0700

    fix: preserve-env

--- dist/install.sh
+++ dist/install.sh
@@ -18,7 +18,7 @@ set -euo pipefail
     echo "Aborting: Direnv version v$(direnv --version) is already installed at $(type -p direnv)"
     exit
   else
-    bin_path="$direnv_bin_path" version="$direnv_version" sudo bash <(curl -L https://raw.githubusercontent.com/direnv/direnv/fe2123fc729b7a6a5954460282810dd226263c4e/install.sh)
+    bin_path="$direnv_bin_path" version="$direnv_version" sudo --preserve-env bash <(curl -L https://raw.githubusercontent.com/direnv/direnv/fe2123fc729b7a6a5954460282810dd226263c4e/install.sh)
   fi
   echo "::endgroup::"
 }
