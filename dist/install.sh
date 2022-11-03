24bafd0b03e3f9ae9f4faba479df31ac550276b8
aabccd021 <aabccd021@gmail.com>
2022-11-03 16:26:20 +0700

    fix: env

--- dist/install.sh
+++ dist/install.sh
@@ -11,16 +11,16 @@ set -euo pipefail
   INPUT_INSTALL_URL="https://releases.nixos.org/nix/nix-2.11.0/install" \
   INPUT_NIX_PATH= \
   GITHUB_PATH=/dev/null \
-  bash <(curl -sfL https://raw.githubusercontent.com/cachix/install-nix-action/11f4ad19be46fd34c005a2864996d8f197fb51c6/install-nix.sh)
+    bash <(curl -sfL https://raw.githubusercontent.com/cachix/install-nix-action/11f4ad19be46fd34c005a2864996d8f197fb51c6/install-nix.sh)
 
   echo "::group::Installing direnv"
   if $(type -p direnv &>/dev/null) && [[ "v$(direnv --version)" == "$direnv_version" ]] ; then
     echo "Aborting: Direnv version v$(direnv --version) is already installed at $(type -p direnv)"
     exit
   else
-    export bin_path="$direnv_bin_path"
-    export version="$direnv_version"
-    bash <(curl -sfL https://raw.githubusercontent.com/direnv/direnv/fe2123fc729b7a6a5954460282810dd226263c4e/install.sh)
+    bin_path="$direnv_bin_path" \
+    version="$direnv_version" \
+      bash <(curl -sfL https://raw.githubusercontent.com/direnv/direnv/fe2123fc729b7a6a5954460282810dd226263c4e/install.sh)
   fi
   echo "::endgroup::"
 }
