57dc68abcd5e9b05b1e53833f8984dd27488dc84
aabccd021 <aabccd021@gmail.com>
2022-11-03 19:58:44 +0700

    fix: mkShellNoCC

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   };
  
   outputs = { self, nixpkgs }: with nixpkgs.legacyPackages.x86_64-linux; {
-    devShell.x86_64-linux = mkShell {
+    devShell.x86_64-linux = mkShellNoCC {
       buildInputs = [
         nodejs-16_x
         nodePackages.pnpm
