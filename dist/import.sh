0fc1d5f899fc07d8365becc99bed4441ecf0799b
aabccd021 <aabccd021@gmail.com>
2022-11-03 14:33:39 +0700

    fix: source profile

--- dist/import.sh
+++ dist/import.sh
@@ -2,6 +2,7 @@
 
 set -euo pipefail
 
+source "$HOME/.profile"
 source $HOME/.nix-profile/share/nix-direnv/direnvrc
 echo "::group::Importing to Nix Store"
 nix copy --no-check-sigs --from $1 ./#devShell.x86_64-linux
