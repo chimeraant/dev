b7e96d3e85c982245a1be5085ddef235833b6788
aabccd021 <aabccd021@gmail.com>
2022-11-03 11:40:50 +0700

    fix: full direnv

--- dist/install.sh
+++ dist/install.sh
@@ -16,11 +16,58 @@ set -euo pipefail
 
   # Stripped version of https://github.com/direnv/direnv/blob/fe2123fc729b7a6a5954460282810dd226263c4e/install.sh
   # Able to pin direnv version and cache the direnv binary
-  echo "::group::Installing direnv"
+
+  log() {
+    echo "[installer] $*" >&2
+  }
+
+  die() {
+    log "$@"
+    exit 1
+  }
+
+  at_exit() {
+    ret=$?
+    if [[ $ret -gt 0 ]]; then
+      log "the script failed with error $ret.\n" \
+        "\n" \
+        "To report installation errors, submit an issue to\n" \
+        "    https://github.com/direnv/direnv/issues/new/choose"
+    fi
+    exit "$ret"
+  }
+  trap at_exit EXIT
+
+  kernel=$(uname -s | tr "[:upper:]" "[:lower:]")
+  case "${kernel}" in
+    mingw*)
+      kernel=windows
+      ;;
+  esac
+  case "$(uname -m)" in
+    x86_64)
+      machine=amd64
+      ;;
+    i686 | i386)
+      machine=386
+      ;;
+    aarch64 | arm64)
+      machine=arm64
+      ;;
+    *)
+      die "Machine $(uname -m) not supported by the installer.\n" \
+        "Go to https://direnv for alternate installation methods."
+      ;;
+  esac
+  log "kernel=$kernel machine=$machine"
+
+  : "${use_sudo:=}"
   : "${bin_path:=}"
-  : "${cache_path:=}"
 
   if [[ -z "$bin_path" ]]; then
+    log "bin_path is not set, you can set bin_path to specify the installation path"
+    log "e.g. export bin_path=/path/to/installation before installing"
+    log "looking for a writeable path from PATH environment variable"
     for path in $(echo "$PATH" | tr ':' '\n'); do
       if [[ -w $path ]]; then
         bin_path=$path
@@ -29,17 +76,45 @@ set -euo pipefail
     done
   fi
   if [[ -z "$bin_path" ]]; then
-    echo "did not find a writeable path in $PATH"
-    exit 1
+    die "did not find a writeable path in $PATH"
   fi
   echo "bin_path=$bin_path"
 
-  if [[ -z "$cache_path" ]]; then
-    curl -o "$bin_path/direnv" -fL "https://github.com/direnv/direnv/releases/download/v2.32.1/direnv.linux-amd64"
+  : "${cache_path:=}"
+  : "${release_id:=latest}"
+
+  cached_bin="$cache_path/$release_id/direnv.$kernel.$machine"
+
+  if [[ -r "$cached_bin"  ]]; then
+    log "found cached binary on $cached_bin"
+    cp "$cached_bin" "$bin_path/direnv"
   else
-    cp "$cache_path" "$bin_path/direnv"
+    log "looking for a download URL"
+    download_url=$(
+      curl -fL "https://api.github.com/repos/direnv/direnv/releases/$release_id" \
+      | grep browser_download_url \
+      | cut -d '"' -f 4 \
+      | grep "direnv.$kernel.$machine"
+    )
+
+    echo "download_url=$download_url"
+
+    log "downloading"
+    curl -o "$bin_path/direnv" -fL "$download_url"
   fi
+
   chmod +x "$bin_path/direnv"
+
+  cat <<DONE
+The direnv binary is now available in:
+    $bin_path/direnv
+The last step is to configure your shell to use it. For example for bash, add
+the following lines at the end of your ~/.bashrc:
+    eval "\$(direnv hook bash)"
+Then restart the shell.
+For other shells, see https://direnv.net/docs/hook.html
+Thanks!
+DONE
 
   LINE='eval "\$(direnv hook bash)"'
   FILE="$HOME/.profile"
