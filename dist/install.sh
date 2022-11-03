6aa7613e996642fa3e69f1e1266ef0f62729cbd3
aabccd021 <aabccd021@gmail.com>
2022-11-03 13:55:18 +0700

    fix: direnv

--- dist/install.sh
+++ dist/install.sh
@@ -10,13 +10,12 @@ set -euo pipefail
   INPUT_EXTRA_NIX_CONFIG= \
   INPUT_INSTALL_OPTIONS= \
   INPUT_INSTALL_URL="https://releases.nixos.org/nix/nix-2.11.0/install" \
-  INPUT_NIX_PATH="nixpkgs=channel:nixos-unstable" \
+  INPUT_NIX_PATH= \
   GITHUB_PATH=/dev/null \
   GITHUB_ENV=/dev/null \
   bash <(curl -sfL https://raw.githubusercontent.com/cachix/install-nix-action/11f4ad19be46fd34c005a2864996d8f197fb51c6/install-nix.sh)
 
-  nix-env --install --attr direnv nix-direnv -f '<nixpkgs>'
-  echo "source $HOME/.nix-profile/share/nix-direnv/direnvrc" >> $HOME/.direnvrc
+  nix-env -iA direnv -f https://github.com/NixOS/nixpkgs/archive/ee01de29d2f58d56b1be4ae24c24bd91c5380cea.tar.gz
 
   LINE='eval "\$(direnv hook bash)"'
   FILE="$HOME/.profile"
