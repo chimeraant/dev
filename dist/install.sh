45871ff6d6b606810ff67a1a66590c26ba28be14
aabccd021 <aabccd021@gmail.com>
2022-11-03 16:03:31 +0700

    fix: bin_path

--- dist/install.sh
+++ dist/install.sh
@@ -18,7 +18,7 @@ set -euo pipefail
     echo "Aborting: Direnv version v$(direnv --version) is already installed at $(type -p direnv)"
     exit
   else
-    sudo --preserve-env bash <(curl -L https://raw.githubusercontent.com/direnv/direnv/fe2123fc729b7a6a5954460282810dd226263c4e/install.sh)
+    bin_path="$direnv_bin_path" sudo --preserve-env bash <(curl -L https://raw.githubusercontent.com/direnv/direnv/fe2123fc729b7a6a5954460282810dd226263c4e/install.sh)
   fi
   echo "::endgroup::"
 }
