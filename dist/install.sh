3b2c4374c4c72e8c3a62483177e5fa423393f0f7
aabccd021 <aabccd021@gmail.com>
2022-11-03 13:52:11 +0700

    fix: fix nixpkgs

--- dist/install.sh
+++ dist/install.sh
@@ -10,7 +10,7 @@ set -euo pipefail
   INPUT_EXTRA_NIX_CONFIG= \
   INPUT_INSTALL_OPTIONS= \
   INPUT_INSTALL_URL="https://releases.nixos.org/nix/nix-2.11.0/install" \
-  INPUT_NIX_PATH="ixpkgs=channel:nixos-unstable" \
+  INPUT_NIX_PATH="nixpkgs=channel:nixos-unstable" \
   GITHUB_PATH=/dev/null \
   GITHUB_ENV=/dev/null \
   bash <(curl -sfL https://raw.githubusercontent.com/cachix/install-nix-action/11f4ad19be46fd34c005a2864996d8f197fb51c6/install-nix.sh)
