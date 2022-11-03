cdaa4f291c0b30f3e1ec20a566b9798103cd3f11
aabccd021 <aabccd021@gmail.com>
2022-11-03 20:04:34 +0700

    fix: minimalMkShell

--- flake.nix
+++ flake.nix
@@ -2,32 +2,47 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
   };
- 
-  outputs = { self, nixpkgs }: with nixpkgs.legacyPackages.x86_64-linux; {
-    devShell.x86_64-linux = mkShellNoCC {
-      buildInputs = [
-        nodejs-16_x
-        nodePackages.pnpm
-        playwright
-        playwright.browsers-chromium
-      ];
 
-      shellHook = ''
-        export PLAYWRIGHT_BROWSERS_PATH=${playwright.browsers-chromium}
-        export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
-        function assertEqual {
-          result="$(eval "$1")"
-          if [[ "$result" != "$2" ]]; then
-            echo -e "\033[0;31m"
-            echo -e "Invalid $1"
-            echo -e "Actual: $result"
-            echo -e "Expected: $2"
-            echo -e "\033[0m"
-          fi
-        }
-        assertEqual "nix --version" "nix (Nix) 2.11.0" || exit 1
-        [ ! -d "node_modules" ] && pnpm install
-      '';
+  outputs = { self, nixpkgs }: with nixpkgs.legacyPackages.x86_64-linux;
+    let
+      stdenvMinimal = nixpkgs.stdenvNoCC.override {
+        cc = null;
+        preHook = "";
+        allowedRequisites = null;
+        initialPath = nixpkgs.lib.filter
+          (a: nixpkgs.lib.hasPrefix "coreutils" a.name)
+          nixpkgs.stdenvNoCC.initialPath;
+        extraNativeBuildInputs = [ ];
+      };
+      minimalMkShell = nixpkgs.mkShell.override {
+        stdenv = stdenvMinimal;
+      };
+    in
+    {
+      devShell.x86_64-linux = minimalMkShell {
+        buildInputs = [
+          nodejs-16_x
+          nodePackages.pnpm
+          playwright
+          playwright.browsers-chromium
+        ];
+
+        shellHook = ''
+          export PLAYWRIGHT_BROWSERS_PATH=${playwright.browsers-chromium}
+          export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
+          function assertEqual {
+            result="$(eval "$1")"
+            if [[ "$result" != "$2" ]]; then
+              echo -e "\033[0;31m"
+              echo -e "Invalid $1"
+              echo -e "Actual: $result"
+              echo -e "Expected: $2"
+              echo -e "\033[0m"
+            fi
+          }
+          assertEqual "nix --version" "nix (Nix) 2.11.0" || exit 1
+          [ ! -d "node_modules" ] && pnpm install
+        '';
+      };
     };
-  };
 }
