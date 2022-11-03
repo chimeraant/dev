e2845b683fc95b267983c269abc7d9ff1327386d
aabccd021 <aabccd021@gmail.com>
2022-11-03 19:21:14 +0700

    docs: fix docs

--- dist/install.sh
+++ dist/install.sh
@@ -3,9 +3,6 @@
 set -euo pipefail
 
 {
-  # Ignore GITHUB_PATH and GITHUB_ENV for local installation
-  # GITHUB_PATH from the downloaded script is not added to path anyway
-  # Path export is instead done using js toolkit
   export GITHUB_PATH=/dev/null
   export INPUT_EXTRA_NIX_CONFIG=
   export INPUT_INSTALL_OPTIONS=
@@ -13,13 +10,13 @@ set -euo pipefail
   export INPUT_NIX_PATH=
   curl -sfL https://raw.githubusercontent.com/cachix/install-nix-action/11f4ad19be46fd34c005a2864996d8f197fb51c6/install-nix.sh | bash
 
-  echo "::group::Installing direnv"
   export version="v2.32.1"
   if $(type -p direnv &>/dev/null) && [[ "v$(direnv --version)" == "$version" ]] ; then
-    echo "Aborting: Direnv version $version is already installed at $(type -p direnv)"
+    echo "Aborting: Direnv $version is already installed at $(type -p direnv)"
   else
+    echo "::group::Installing direnv"
     export bin_path="/usr/local/bin"
     curl -sfL https://raw.githubusercontent.com/direnv/direnv/db00133c845be3a01437b933936db5aa40977d30/install.sh | sudo --preserve-env bash
+    echo "::endgroup::"
   fi
-  echo "::endgroup::"
 }
