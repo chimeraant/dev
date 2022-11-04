e51a67b013d0c44f2c831ec126a9630d1c4161d8
aabccd021 <aabccd021@gmail.com>
2022-11-04 14:23:17 +0700

    fix: fixed elapsed

--- src/exec.ts
+++ src/exec.ts
@@ -2,12 +2,12 @@ import * as core from '@actions/core';
 import * as exec from '@actions/exec';
 
 export const prettyExec = async (command: string, args?: string[], option?: exec.ExecOptions) => {
-  const start = process.hrtime();
+  const start = performance.now();
   const output = await exec.getExecOutput(command, args, {
     silent: true,
     ...option,
   });
-  const elapsed = (process.hrtime(start)[1] / 10000000).toFixed(0);
+  const elapsed = (performance.now() - start).toFixed(0);
   const code = output.exitCode === 0 ? '' : ` exit code: ${output.exitCode}`;
   const cmdStr = `${command} ${args?.join(' ')}`;
   core.info(`(${elapsed}s) "${cmdStr}" ${code}`);
