73c264438c1287f7554ead4ce879dce52690c860
aabccd021 <aabccd021@gmail.com>
2022-11-04 20:34:27 +0700

    Revert "fix: file://"
    This reverts commit 49daf958e1d78f80d6ae898c9ee44caf11f486ee.

--- src/nix-store.ts
+++ src/nix-store.ts
@@ -3,16 +3,10 @@ import { prettyExec } from './exec';
 const devShellPath = './#devShell.x86_64-linux';
 
 export const importFrom = (nixCachePath: string) =>
-  prettyExec('nix', ['copy', devShellPath, '--from', `file://${nixCachePath}`, '--no-check-sigs']);
+  prettyExec('nix', ['copy', devShellPath, '--from', nixCachePath, '--no-check-sigs']);
 
 export const exportTo = async (nixCachePath: string) => {
   await prettyExec('nix', ['store', 'gc']);
   await prettyExec('nix', ['store', 'optimise']);
-  await prettyExec('nix', [
-    'copy',
-    devShellPath,
-    '--to',
-    `file://${nixCachePath}`,
-    '--no-check-sigs',
-  ]);
+  await prettyExec('nix', ['copy', devShellPath, '--to', nixCachePath, '--no-check-sigs']);
 };
