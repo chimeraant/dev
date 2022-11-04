6f8316c02c6143fc676addf804012e914d63497a
aabccd021 <aabccd021@gmail.com>
2022-11-04 23:49:49 +0700

    fix: add more path

--- src/setup.ts
+++ src/setup.ts
@@ -16,6 +16,7 @@ const restoreNixCache = async () => {
 export const setup = async () => {
   // https://github.com/cachix/install-nix-action/blob/11f4ad19be46fd34c005a2864996d8f197fb51c6/install-nix.sh#L84-L85
   core.addPath(`/nix/var/nix/profiles/default/bin`);
+  core.addPath(`/nix/var/nix/profiles/per-user/${process.env['USER']}/profile/bin`);
   await Promise.all([restoreNixCache(), restoreCache(pnpmCache)]);
   await prettyExec('direnv', ['allow']);
   const { stdout } = await prettyExec('direnv', ['export', 'json']);
