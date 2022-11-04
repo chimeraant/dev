38c7871e7d486507a33cac1d1cf7c2bc2ba0cf7b
aabccd021 <aabccd021@gmail.com>
2022-11-04 15:18:22 +0700

    fix: single user nix installation

--- install.sh
+++ install.sh
@@ -3,12 +3,45 @@
 set -euo pipefail
 
 {
-  export GITHUB_PATH=/dev/null
-  export INPUT_EXTRA_NIX_CONFIG=
-  export INPUT_INSTALL_OPTIONS=
-  export INPUT_INSTALL_URL="https://releases.nixos.org/nix/nix-2.11.0/install"
-  export INPUT_NIX_PATH=
-  curl -sfL https://raw.githubusercontent.com/cachix/install-nix-action/11f4ad19be46fd34c005a2864996d8f197fb51c6/install-nix.sh | bash
+  if type -p nix &>/dev/null ; then
+    echo "Aborting: Nix is already installed at $(type -p nix)"
+    exit
+  fi
+
+  workdir=$(mktemp -d)
+  trap 'rm -rf "$workdir"' EXIT
+
+  add_config() {
+    echo "$1" | tee -a "$workdir/nix.conf" >/dev/null
+  }
+  add_config "max-jobs = auto"
+  add_config "trusted-users = root $USER"
+  add_config "experimental-features = nix-command flakes"
+
+  installer_options=(
+  )
+
+  add_config "build-users-group ="
+  sudo mkdir -p /etc/nix
+  sudo chmod 0755 /etc/nix
+  sudo cp $workdir/nix.conf /etc/nix/nix.conf
+
+  # There is --retry-on-errors, but only newer curl versions support that
+  curl_retries=5
+  while ! curl -sS -o "$workdir/install" -v --fail -L "https://releases.nixos.org/nix/nix-2.11.0/install"
+  do
+    sleep 1
+    ((curl_retries--))
+    if [[ $curl_retries -le 0 ]]; then
+      echo "curl retries failed" >&2
+      exit 1
+    fi
+  done
+
+  sh "$workdir/install" \
+    --no-channel-add \
+    --darwin-use-unencrypted-nix-store-volume \
+    --nix-extra-conf-file "$workdir/nix.conf" \
 
   export version="v2.32.1"
   if $(type -p direnv &>/dev/null) && [[ "v$(direnv --version)" == "$version" ]] ; then
