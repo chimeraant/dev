48cd5b20b44252c1c81dc7f506faed843f6dbcf4
aabccd021 <aabccd021@gmail.com>
2022-11-04 20:22:32 +0700

    Revert "fix: from itself"
    This reverts commit 182b9acf904ee1cefdd079853a45e0f355ed4b89.

--- src/nix-store.ts
+++ src/nix-store.ts
@@ -3,10 +3,10 @@ import { prettyExec } from './exec';
 const devShellPath = './#devShell.x86_64-linux';
 
 export const importFrom = (_nixCachePath: string) =>
-  prettyExec('nix', ['copy', devShellPath, '--from', '/', '--no-check-sigs']);
+  prettyExec('nix', ['copy', devShellPath, '--from', '/nix/store', '--no-check-sigs']);
 
 export const exportTo = async (_nixCachePath: string) => {
   await prettyExec('nix', ['store', 'gc']);
   await prettyExec('nix', ['store', 'optimise']);
-  await prettyExec('nix', ['copy', devShellPath, '--to', '/', '--no-check-sigs']);
+  await prettyExec('nix', ['copy', devShellPath, '--to', '/nix/store', '--no-check-sigs']);
 };
