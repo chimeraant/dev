4f6e6ec603967ab4c99b60afbd5548c24c8340b5
aabccd021 <aabccd021@gmail.com>
2022-11-04 20:46:24 +0700

    fix: print all buffer

--- dist/index.js
+++ dist/index.js
@@ -216,20 +216,21 @@ const exec = __importStar(__nccwpck_require__(5082));
 const prettyExec = async (command, args, option) => {
     const cmdStr = `${command} ${args?.join(' ') ?? ''}`;
     console.time(`>>> "${cmdStr}"`);
+    const buffers = [];
     const output = await exec.getExecOutput(command, args, {
         silent: true,
         ignoreReturnCode: true,
+        listeners: {
+            stdline: (s) => buffers.push(`[stdout] ${s}`),
+            stderr: (s) => buffers.push(`[stderr] ${s}`),
+        },
         ...option,
     });
     const code = output.exitCode === 0 ? '' : ` exit code: ${output.exitCode}`;
     console.timeEnd(`>>> "${cmdStr}"`);
-    core.startGroup(`stderr`);
-    core.info(output.stderr);
+    core.startGroup(`>>> "${cmdStr}"`);
+    buffers.forEach(core.info);
     core.endGroup();
-    core.startGroup(`stdout`);
-    core.info(output.stdout);
-    core.endGroup();
-    core.info('\n');
     if (output.exitCode !== 0) {
         throw Error(`Error: The process "${cmdStr}" failed with exit code: ${code}`);
     }
--- src/exec.ts
+++ src/exec.ts
@@ -4,20 +4,21 @@ import * as exec from '@actions/exec';
 export const prettyExec = async (command: string, args?: string[], option?: exec.ExecOptions) => {
   const cmdStr = `${command} ${args?.join(' ') ?? ''}`;
   console.time(`>>> "${cmdStr}"`);
+  const buffers: string[] = [];
   const output = await exec.getExecOutput(command, args, {
     silent: true,
     ignoreReturnCode: true,
+    listeners: {
+      stdline: (s) => buffers.push(`[stdout] ${s}`),
+      stderr: (s) => buffers.push(`[stderr] ${s}`),
+    },
     ...option,
   });
   const code = output.exitCode === 0 ? '' : ` exit code: ${output.exitCode}`;
   console.timeEnd(`>>> "${cmdStr}"`);
-  core.startGroup(`stderr`);
-  core.info(output.stderr);
+  core.startGroup(`>>> "${cmdStr}"`);
+  buffers.forEach(core.info);
   core.endGroup();
-  core.startGroup(`stdout`);
-  core.info(output.stdout);
-  core.endGroup();
-  core.info('\n');
   if (output.exitCode !== 0) {
     throw Error(`Error: The process "${cmdStr}" failed with exit code: ${code}`);
   }
