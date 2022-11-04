5334fa15e1fe435261e3f4d0c2eb1f1190928731
aabccd021 <aabccd021@gmail.com>
2022-11-04 17:30:41 +0700

    fix: fixed installer

--- install.sh
+++ install.sh
@@ -5,19 +5,14 @@ set -euo pipefail
 {
 
   export nix_version="2.11.0"
-  if $(type -p nix &>/dev/null) && [[ "nix (Nix) $(nix --version)" == "$nix_version" ]] ; then
+  if $(type -p nix &>/dev/null) && [[ $(nix --version) == "nix (Nix) $nix_version" ]] ; then
     echo "nix $nix_version is already installed at $(type -p nix). Skipping installation."
   else
     sudo mkdir -p /etc/nix
     sudo chmod 0755 /etc/nix
     sudo sh -c 'printf "max-jobs = auto\ntrusted-users = $USER\nexperimental-features = nix-command flakes" >> /etc/nix/nix.conf'
-
-    # cache
-    if [[ ! -d /tmp/nix ]]; then
-      mkdir -p /tmp/nix
-      curl -o /tmp/nix/install -sfL "https://releases.nixos.org/nix/nix-$nix_version/install"
-    fi
-    sh /tmp/nix/install --no-channel-add --nix-extra-conf-file /etc/nix/nix.conf
+    sh <(curl -o /tmp/nix/install -sfL "https://releases.nixos.org/nix/nix-$nix_version/install") \
+      --no-channel-add --nix-extra-conf-file /etc/nix/nix.conf
   fi
 
   export version="v2.32.1"
