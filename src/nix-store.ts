4347c731a86da93459f3b34f86eb8aef7eb600ab
aabccd021 <aabccd021@gmail.com>
2022-11-04 20:10:06 +0700

    fix: nix store import from itself

--- src/nix-store.ts
+++ src/nix-store.ts
@@ -2,11 +2,11 @@ import { prettyExec } from './exec';
 
 const devShellPath = './#devShell.x86_64-linux';
 
-export const importFrom = (nixCachePath: string) =>
-  prettyExec('nix', ['copy', devShellPath, '--from', nixCachePath, '--no-check-sigs']);
+export const importFrom = (_nixCachePath: string) =>
+  prettyExec('nix', ['copy', devShellPath, '--from', '/nix/store', '--no-check-sigs']);
 
-export const exportTo = async (nixCachePath: string) => {
+export const exportTo = async (_nixCachePath: string) => {
   await prettyExec('nix', ['store', 'gc']);
   await prettyExec('nix', ['store', 'optimise']);
-  await prettyExec('nix', ['copy', devShellPath, '--to', nixCachePath, '--no-check-sigs']);
+  await prettyExec('nix', ['copy', devShellPath, '--to', '/nix/store', '--no-check-sigs']);
 };
