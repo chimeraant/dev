4cc764345b0ddefbc4bc415ae9bd3e158e02e797
aabccd021 <aabccd021@gmail.com>
2022-11-04 14:37:15 +0700

    fix: real time direnv

--- src/direnv.ts
+++ src/direnv.ts
@@ -1,15 +1,14 @@
 import * as core from '@actions/core';
+import * as exec from '@actions/exec';
 
 import { prettyExec } from './exec';
 
 const exportVariables = async () => {
-  const { stdout } = await prettyExec('direnv', ['export', 'json']);
+  const { stdout } = await exec.getExecOutput('direnv', ['export', 'json']);
   Object.entries(JSON.parse(stdout)).forEach(([key, value]) => core.exportVariable(key, value));
 };
 
-const allow = () => prettyExec('direnv', ['allow']);
-
 export const setup = async () => {
-  await allow();
+  await prettyExec('direnv', ['allow']);
   await exportVariables();
 };
